e2f9fab74a9090d95d5ebe2e8a20f7437c4443e4
Devin Mounts and Ryan Murry <devin@>
2018-06-27 11:23:06 -0700

    create anagram class, setter and getters, and method to create and sort array.

---
+++ Language.Tests/Anagram.Tests.cs
@@ -0,0 +1,67 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Language.Models;
+
+namespace Language.Tests
+{
+    [TestClass]
+    public class AnagramTests
+    {
+        [TestMethod]
+        public void GetTargetString_ReturnsTargetString_String()
+        {
+            //Arrange
+            string targetString = "tea";
+            Anagram inputAnagram = new Anagram(targetString);
+
+            //Act
+            string result = inputAnagram.GetTargetString();
+
+            //Assert
+            Assert.AreEqual(targetString, result);
+        }
+
+        [TestMethod]
+        public void SetTargetString_SetTargetString_String()
+        {
+            //Arrange
+            string targetString = "tea";
+            Anagram inputAnagram = new Anagram(targetString);
+
+            //Act
+            string newTargetString = "beat";
+            inputAnagram.SetTargetString(newTargetString);
+            string result = inputAnagram.GetTargetString();
+
+            //Assert
+            Assert.AreEqual(newTargetString, result);
+        }
+
+        [TestMethod]
+        public void SortArray_ReturnsTargetArray_Char()
+        {
+            //Arrange
+            string targetString = "tea";
+            Anagram inputAnagram = new Anagram(targetString);
+
+            //Act
+            char[] inputArray = inputAnagram.SortArray(targetString);
+
+            //Assert
+            Assert.AreEqual(3, inputArray.Length);
+        }
+
+        [TestMethod]
+        public void SortArray_ReturnsSortedArray_Char()
+        {
+            //Arrange
+            string targetString = "tea";
+            Anagram inputAnagram = new Anagram(targetString);
+
+            //Act
+            char[] sortedArray = inputAnagram.SortArray(targetString);
+        
+            //Assert
+            Assert.AreEqual('a', sortedArray[0]);
+        }
+    }
+}
---
+++ Language/Anagram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Language.Models
+{
+    public class Anagram
+    {
+        private string _targetString;
+
+        public Anagram(string targetString)
+        {
+            _targetString = targetString;
+        }
+
+
+        public void SetTargetString(string newTargetString)
+        {
+            _targetString = newTargetString;
+        }
+
+        public string GetTargetString()
+        {
+            return _targetString;
+        }
+
+        public char[] SortArray(string targetString)
+        {
+            char[] targetArray = targetString.ToCharArray();
+            Array.Sort(targetArray);
+            return targetArray;
+        }
+    }
+}
