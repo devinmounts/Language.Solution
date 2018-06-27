8987cb659ae687ed5de1437cea947ba39c4596e7
Devin Mounts and Ryan Murry <devin@>
2018-06-27 16:32:29 -0700

    add comments to program.cs

--- Language/Program.cs
+++ Language/Program.cs
@@ -12,7 +12,7 @@ namespace Language
             string initial = Console.ReadLine().ToLower();
             if (initial == "y")
             {
-                //Prompt user for target word, convert to lower case, and store in object
+                //prompt user for target word, convert to lower case, and store in object
                 Console.WriteLine("Enter a word to test for Anagram.");
                 string targetString = Console.ReadLine().ToLower();
                 Anagram targetAnagram = new Anagram(targetString);
@@ -22,7 +22,7 @@ namespace Language
                 string testString;
                 bool prompt = true;
 
-                //Continually prompt user for test words until they quit
+                //continually prompt user for test words until they quit
                 do
                 {
                     Console.WriteLine("Enter a word to compare: ");
@@ -40,8 +40,10 @@ namespace Language
 
                 } while (prompt == true);
 
+                //get list all test words have been added to
                 List<string> testList = Anagram.GetAll();
 
+                //check if each test word in list is an anagram of the target word
                 for (int i = 0; i < testList.Count; i++)
                 {
                     if (targetAnagram.CheckAnagram(targetString, i))
@@ -54,6 +56,8 @@ namespace Language
                     }
                        
                 }
+
+                //prompt the user to play again
                 Console.WriteLine("Do you want to play again (Y/N)");
                 string playAgain = Console.ReadLine().ToLower();
                 if (playAgain == "y")
