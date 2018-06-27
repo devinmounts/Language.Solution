e034e5648b87bdd5d95bd1095e5c0fa1255ccb8f
Devin Mounts and Ryan Murry <devin@>
2018-06-27 14:48:19 -0700

    populate a list of test words and convert them to arrays.  It will then compare test words to target words and return true false for anagram.

--- Language.Tests/Anagram.Tests.cs
+++ Language.Tests/Anagram.Tests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using Language.Models;
 
 namespace Language.Tests
 {
     [TestClass]
-    public class AnagramTests
+    public class AnagramTests : IDisposable
     {
+        public void Dispose()
+        {
+            Anagram.ClearAll();   
+        }
+
         [TestMethod]
         public void GetTargetString_ReturnsTargetString_String()
         {
@@ -59,9 +66,171 @@ namespace Language.Tests
 
             //Act
             char[] sortedArray = inputAnagram.SortArray(targetString);
-        
+
             //Assert
             Assert.AreEqual('a', sortedArray[0]);
+        }
+
+        [TestMethod]
+        public void CheckAnagram_IsWordAnAnagram_True()
+        {
+            //Arange
+            string targetString = "tea";
+            Anagram targetAnagram = new Anagram(targetString);
+            string testString01 = "eat";
+            string testString02 = "eater";
+            string testString03 = "cat";
+            Anagram testAnagram01 = new Anagram(testString01);
+            testAnagram01.Save(testString01);
+            Anagram testAnagram02 = new Anagram(testString02);
+            testAnagram02.Save(testString02);
+            Anagram testAnagram03 = new Anagram(testString03);
+            testAnagram03.Save(testString03);
+
+            //Act
+            bool result = targetAnagram.CheckAnagram(targetString, 0);
+
+
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void CheckAnagram_IsWordDifferentLength_False()
+        {
+            //Arange
+            string targetString = "tea";
+            Anagram targetAnagram = new Anagram(targetString);
+            string testString01 = "eat";
+            string testString02 = "eater";
+            string testString03 = "cat";
+            Anagram testAnagram01 = new Anagram(testString01);
+            testAnagram01.Save(testString01);
+            Anagram testAnagram02 = new Anagram(testString02);
+            testAnagram02.Save(testString02);
+            Anagram testAnagram03 = new Anagram(testString03);
+            testAnagram03.Save(testString03);
+
+            //Act
+            bool result = targetAnagram.CheckAnagram(targetString, 1);
+
+
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void CheckAnagram_IsWordNotAnAnagram_False()
+        {
+            //Arange
+            string targetString = "tea";
+            Anagram targetAnagram = new Anagram(targetString);
+            string testString01 = "eat";
+            string testString02 = "eater";
+            string testString03 = "cat";
+            Anagram testAnagram01 = new Anagram(testString01);
+            testAnagram01.Save(testString01);
+            Anagram testAnagram02 = new Anagram(testString02);
+            testAnagram02.Save(testString02);
+            Anagram testAnagram03 = new Anagram(testString03);
+            testAnagram03.Save(testString03);
+
+            //Act
+            bool result = targetAnagram.CheckAnagram(targetString, 2);
+
+
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+
+
+        //[TestMethod]
+        //public void CheckAnagram_IsWordAnAnagram_True()
+        //{
+        //    //Arrange
+        //    string targetString = "tea";
+        //    Anagram inputAnagram = new Anagram(targetString);
+
+        //    //Act
+        //    char[] sortedArray = inputAnagram.SortArray(targetString);
+        //    bool result = inputAnagram.CheckAnagram(sortedArray, "eat");
+
+        //    //Assert
+        //    Assert.AreEqual(true, result);
+        //}
+
+        //[TestMethod]
+        //public void CheckAnagram_IsWordDifferentLength_False()
+        //{
+        //    //Arrange
+        //    string targetString = "tea";
+        //    Anagram inputAnagram = new Anagram(targetString);
+
+        //    //Act
+        //    char[] sortedArray = inputAnagram.SortArray(targetString);
+        //    bool result = inputAnagram.CheckAnagram(sortedArray, "eater");
+
+        //    //Assert
+        //    Assert.AreEqual(false, result);
+        //}
+
+        //[TestMethod]
+        //public void CheckAnagram_IsWordNotAnAnagram_False()
+        //{
+        //    //Arrange
+        //    string targetString = "tea";
+        //    Anagram inputAnagram = new Anagram(targetString);
+
+        //    //Act
+        //    char[] sortedArray = inputAnagram.SortArray(targetString);
+        //    bool result = inputAnagram.CheckAnagram(sortedArray, "cat");
+
+        //    //Assert
+        //    Assert.AreEqual(false, result);
+        //}
+
+        [TestMethod]
+        public void Save_AddInputToList_String()
+        {
+            //Arange
+            string testString01 = "eat";
+            string testString02 = "eater";
+            Anagram testAnagram01 = new Anagram(testString01);
+            testAnagram01.Save(testString01);
+            Anagram testAnagram02 = new Anagram(testString02);
+            testAnagram02.Save(testString02);
+            List<string> newList = new List<string> { testString01, testString02 };
+
+            //Act
+            List<string> result = Anagram.GetAll();
+
+            //Assert
+            CollectionAssert.AreEqual(newList, result);
+        }
+
+        [TestMethod]
+        public void CheckAnagram_CompareListElementToTargetWord_True()
+        {
+            //Arange
+            string targetString = "tea";
+            Anagram targetAnagram = new Anagram(targetString);
+            string testString01 = "eat";
+            string testString02 = "eater";
+            string testString03 = "cat";
+            Anagram testAnagram01 = new Anagram(testString01);
+            testAnagram01.Save(testString01);
+            Anagram testAnagram02 = new Anagram(testString02);
+            testAnagram02.Save(testString02);
+            Anagram testAnagram03 = new Anagram(testString03);
+            testAnagram03.Save(testString03);
+
+            //Act
+            bool result = targetAnagram.CheckAnagram(targetString, 0);
+
+
+            //Assert
+            Assert.AreEqual(true, result);
         }
     }
 }
--- Language/Anagram.cs
+++ Language/Anagram.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Language.Models
 {
     public class Anagram
     {
         private string _targetString;
+        private static List<string> _testWord = new List<string> { };
 
         public Anagram(string targetString)
         {
@@ -22,11 +24,52 @@ namespace Language.Models
             return _targetString;
         }
 
+        public void Save(string input)
+        {
+            _testWord.Add(input);
+        }
+
+        public static void ClearAll()
+        {
+            _testWord.Clear();
+        }
+        public static List<string> GetAll()
+        {
+            return _testWord;
+        }
+
         public char[] SortArray(string targetString)
         {
             char[] targetArray = targetString.ToCharArray();
             Array.Sort(targetArray);
             return targetArray;
+        }
+
+        public bool CheckAnagram(string targetString, int index)
+        {
+            char[] sortedTargetArray = SortArray(targetString);
+            string testString = _testWord[index];
+            char[] sortedTestArray = SortArray(testString);
+            if (sortedTargetArray.Length != sortedTestArray.Length)
+            {
+                return false;
+
+            }
+            int i = 0;
+            while (i < sortedTargetArray.Length)
+            {
+                if (sortedTargetArray[i] == sortedTestArray[i])
+                {
+                    i++;
+                }
+                else
+                {
+                    return false;
+
+                }
+            }
+            return true;
+
         }
     }
 }
